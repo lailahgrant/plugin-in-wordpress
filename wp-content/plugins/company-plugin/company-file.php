9fb2ee67e39d5f93952f76f76913754ba0fe2b52
lailahgrant <lailahgrant19@gmail.com>
2022-01-04 11:13:19 +0300

    company plugin infor

---
+++ wp-content/plugins/company-plugin/company-file.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:     Company People Overview
+ * Plugin URI:      https://google.com
+ * Description:     A new overview of all person in the client's company, Gutenberg Block Type that takes in Company Form Details and displays them on the browser
+ * Version:         1.0.0
+ * Author:          Lailah Grant
+ * Author URi:      https://github.com/lailahgrant
+ * License:         GPL v2 or later
+ * License URI:     
+ * Text Domain:     company-people-overview
+ */
